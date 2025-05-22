00df9f1782389afa9286e1a8a76522f4759b6cd1
namaelsan <x.arybidx@gmail.com>
2025-05-22 19:54:40 +0300

    fullstack basic application init

---
+++ backend/routes/product.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from 'mongoose';
+import Product from "../models/product.model.js";
+import { getProducts, updateProduct, createProduct, deleteProduct } from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+export default router;
+
+router.get("/", getProducts)
+
+router.post("/", createProduct);
+
+router.patch("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
